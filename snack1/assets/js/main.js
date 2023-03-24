53bbf0dddbf1e6364f646369f3916826305b113b
LookasWasTaken~ <piacen.luca@gmail.com>
2023-03-24 12:11:51 +0100

    snack1

---
+++ snack1/assets/js/main.js
@@ -0,0 +1,24 @@
+/* Snack 1:
+Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine. */
+
+// Ho chiesto a chatGPT di generarmi varietà, peso e lunghezza di 10 zucchine
+const zucchine = [
+  { varietà: "Zucchino nero di Milano", peso: 0.5, lunghezza: 15 },
+  { varietà: "Zucchino romanesco", peso: 0.3, lunghezza: 10 },
+  { varietà: "Zucchino siciliano", peso: 0.4, lunghezza: 12 },
+  { varietà: "Zucchino tondo di Piacenza", peso: 0.6, lunghezza: 18 },
+  { varietà: "Zucchina lunga fiorentina", peso: 0.4, lunghezza: 20 },
+  { varietà: "Zucchina striata di Napoli", peso: 0.5, lunghezza: 16 },
+  { varietà: "Zucchina bianca triestina", peso: 0.3, lunghezza: 14 },
+  { varietà: "Zucchino giallo", peso: 0.2, lunghezza: 8 },
+  { varietà: "Zucchina trombetta di Albenga", peso: 0.8, lunghezza: 25 },
+  { varietà: "Zucchino rampicante", peso: 0.7, lunghezza: 22 }
+];
+
+let somma = 0;
+
+zucchine.forEach(zucchina => {
+  somma += zucchina.peso;
+});
+
+console.log("Le zucchine complessivamente pesano", somma)
